fac808cae40e42fe102791d3fb214b12d491326e
kevinmlew <kevin.martin.lew@gmail.com>
2016-06-05 16:16:18 -0300

    Initial commit

---
+++ src/Cliente.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : ConcuChat.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello Cliente!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
